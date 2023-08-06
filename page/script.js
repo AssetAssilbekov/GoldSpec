8894f86f60621a7bac36a021195b2a6be84ad866
AssetAssilbekov <88421733+AssetAssilbekov@users.noreply.github.com>
2023-08-06 14:33:23 +0600

    Update script.js

--- page/script.js
+++ page/script.js
@@ -132,7 +132,7 @@ search.addEventListener('input', ()=> {
   let val = search.value;
   database.forEach((item, i) => {
     if(item.name.toLowerCase().includes(val.toLowerCase())&&num<5&&val!='') {
-      autocomplete.innerHTML += `<li><a href="/page/index.html?id=${item.address}" style="color:black;text-decoration:none;">${item.name}</a></li>`;
+      autocomplete.innerHTML += `<li><a href="https://assetassilbekov.github.io/GoldSpec/page/index.html?id=${item.address}" style="color:black;text-decoration:none;">${item.name}<span style="float:right;font-weight:normal;">2023</span></a></li>`;
       num++;
     };
   });
