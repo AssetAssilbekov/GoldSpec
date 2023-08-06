b099e5b172d8acb74362e083ebc02ccc079c0544
AssetAssilbekov <88421733+AssetAssilbekov@users.noreply.github.com>
2023-08-06 11:38:23 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
 const search = document.querySelector('input');
 const autocomplete = document.querySelector('#autocomplete');
-class Company {
-  constructor(name,logo,address) {
-    this.name = name;
-    this.logo = logo;
-    this.address = address;
-  }
-}
-const companies = [new Company('TSM','','')];
 
 
 search.addEventListener('input', ()=> {
   autocomplete.innerHTML = '';
   let num = 0;
   let val = search.value;
-  companies.forEach((item, i) => {
+  database.forEach((item, i) => {
     if(item.name.toLowerCase().includes(val.toLowerCase())&&num<5&&val!='') {
       autocomplete.innerHTML += `<li><a href="https://assetassilbekov.github.io/GoldSpec/page/index.html?id=${item.address}" style="color:black;text-decoration:none;">${item.name}</a></li>`;
       num++;
