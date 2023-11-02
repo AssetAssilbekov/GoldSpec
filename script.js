32bcd00c15b28c99e2c6af9b4d05e1bc56b94a66
AssetAssilbekov <88421733+AssetAssilbekov@users.noreply.github.com>
2023-11-02 10:04:16 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const search = document.querySelector('input');
 const autocomplete = document.querySelector('#autocomplete');
 
+search.addEventListener('focus', () => {
+  database.forEach((item, i) => {
+      autocomplete.innerHTML += `<li><a href="https://assetassilbekov.github.io/GoldSpec/page/index.html?id=${item.address}" style="color:black;text-decoration:none;">${item.name}<span style="float:right;font-weight:normal;">2023</span></a></li>`;
+    };
+  });
+});
 
 search.addEventListener('input', ()=> {
   autocomplete.innerHTML = '';
